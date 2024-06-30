36dd7047d1a8368d5c3458c73dcde10863b49349
Arkalithe <arkalithe@gmail.com>
2024-06-30 09:50:18 +0200

    ajout du responsive

--- wave.js
+++ wave.js
@@ -1,21 +1,27 @@
 function initWaveAnimation(firstWaveColor, secondWaveColor, waveAmplitude, waveFrequency, fillSpeed, angleSpeed) {
-  // Creation,definition et ajout dans le dom du canvas
+  // Creation, definition et ajout dans le DOM du canvas
+  const canvasWrapper = document.createElement("div");
+  canvasWrapper.className = "canvasWrapper";
   const newCanvas = document.createElement("canvas");
   const newCanvasId = "waveCanvas" + document.querySelectorAll("canvas").length;
   newCanvas.id = newCanvasId;
-  newCanvas.width = 500;
-  newCanvas.height = 500;
-  document.body.appendChild(newCanvas);
+  canvasWrapper.appendChild(newCanvas);
+  document.getElementById('canvasContainer').appendChild(canvasWrapper);
 
-  const canvas = newCanvas  ;
+  const canvas = newCanvas;
   const ctx = canvas.getContext("2d");
-  const width = canvas.width;
-  const height = canvas.height;
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const radius = Math.min(width, height) / 2 - 50; // Pour ajuster le rayon du cercle
+
+  let width, height, centerX, centerY, radius;
   let angle = 0;
   let fillHeight = 0;
+
+  function resizeCanvas() {
+    width = canvas.width = canvasWrapper.clientWidth;
+    height = canvas.height = canvasWrapper.clientHeight;
+    centerX = width / 2;
+    centerY = height / 2;
+    radius = Math.min(width, height) / 2 - 50; // Pour ajuster le rayon du cercle
+  }
 
   function drawCircle() {
     ctx.beginPath();
@@ -32,7 +38,7 @@ function initWaveAnimation(firstWaveColor, secondWaveColor, waveAmplitude, waveF
     ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
     ctx.clip();
 
-    //Vague en premier Plan
+    // Vague en premier plan
     ctx.beginPath();
     for (let x = -radius - 50; x <= radius + 50; x++) {
       const y1 = Math.sin((x + angle) * waveFrequency) * waveAmplitude + radius - fillHeight;
@@ -44,7 +50,7 @@ function initWaveAnimation(firstWaveColor, secondWaveColor, waveAmplitude, waveF
     ctx.fillStyle = firstWaveColor; // format "rgba(x,x,x,x)"
     ctx.fill();
 
-    //Vague en deuxieme plan
+    // Vague en deuxieme plan
     ctx.beginPath();
     for (let x = -radius - 50; x <= radius + 50; x++) {
       const y2 = Math.sin((x - angle) * waveFrequency) * waveAmplitude + radius - fillHeight;
@@ -53,7 +59,7 @@ function initWaveAnimation(firstWaveColor, secondWaveColor, waveAmplitude, waveF
     ctx.lineTo(centerX + radius + 50, centerY + radius);
     ctx.lineTo(centerX - radius - 50, centerY + radius);
     ctx.closePath();
-    ctx.fillStyle = secondWaveColor; // format"rgba(x,x,x,x)"
+    ctx.fillStyle = secondWaveColor; // format "rgba(x,x,x,x)"
     ctx.fill();
 
     ctx.restore();
@@ -70,7 +76,10 @@ function initWaveAnimation(firstWaveColor, secondWaveColor, waveAmplitude, waveF
     requestAnimationFrame(animate);
   }
 
+  window.addEventListener('resize', resizeCanvas);
+  resizeCanvas();
   animate();
 }
+
 initWaveAnimation('rgba(0,100,200,1)', 'rgba(0,150,255,1)', 20, 0.02, 0.5, 10);
 initWaveAnimation('rgba(255,100,0,1)', 'rgba(255,150,0,1)', 25, 0.03, 2, 10);
